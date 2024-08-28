7b75df74931c89915393f814260c501936f479fb
felmar <felmar@192.168.254.169>
2024-08-29 01:06:08 +0800

    CHAt

---
+++ src/loopss/Loopss.java
@@ -0,0 +1,41 @@
+
+package loopss;
+
+import java.util.Scanner;
+
+public class Loopss {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        // if loop
+//        int x = 5;
+//        if(x < 4){
+//            System.out.println("Yes the condition is true");
+//        }
+        
+        // while loop
+        
+        
+//        while(true){
+//            System.out.println("Condition is True");
+//            
+//            System.out.println("Enter x to exit");
+//            String scan = scn.nextLine();
+//            if (scan == "exit"){
+//                break;
+//            }
+//            
+//            
+//        }
+
+
+         // for loop (Three conditions)
+//         for(int i = 0; i <= 5; i++){
+//             System.out.println(i + " times");
+//         }
+//        
+//        
+//    }
+    
+}
+}
